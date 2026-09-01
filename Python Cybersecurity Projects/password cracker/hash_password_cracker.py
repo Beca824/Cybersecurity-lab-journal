54d98fd69c50605df0aa751f2f9eb9e87763d427
Beca824 <beckyafyai@gmail.com>
2026-09-01 15:20:03 +0300

    Updates

--- Python Cybersecurity Projects/password cracker/hash_password_cracker.py
+++ Python Cybersecurity Projects/password cracker/hash_password_cracker.py
@@ -7,18 +7,24 @@
 import hashlib
 from pathlib import Path
 
-target_hash = "5fc458731c2a36c7c1a96099158d709ed7209737617592202b54b1b30effcce8"
+target_hash = input("Enter the target SHA-256 hash: ")
+wordlist_name = input("Enter the wordlist filename: ")
 attempts = 0
 
-with open(Path(__file__).parent / "wordlist.txt", "r") as file:
-    for password in file:
-        password = password.strip()
-        attempts += 1
-        
-        if hashlib.sha256(password.encode()).hexdigest() == target_hash:
-            print(f"Correct! Password: {password} (Attempt {attempts})")
-            break
+print(Path(__file__).parent)
+
+try:
+    with open(Path(__file__).parent / wordlist_name, "r") as file:
+        for password in file:
+                password = password.strip()
+                attempts += 1
+                
+                if hashlib.sha256(password.encode()).hexdigest() == target_hash:
+                    print(f"Correct! Password: {password} (Attempt {attempts})")
+                    break
+                else:
+                    print(f"Attempt {attempts}: {password} - Incorrect")
         else:
-            print(f"Attempt {attempts}: {password} - Incorrect")
-    else:
-        print(f"Password not found. Tried {attempts} passwords.")
+             print(f"Password not found. Tried {attempts} passwords.") 
+except FileNotFoundError:
+                    print("[ERROR] Wordlist file not found.")
---
+++ Python Cybersecurity Projects/password cracker/password_hashing.py
@@ -0,0 +1,18 @@
+# Password Hashing & Salting
+# Python Cybersecurity Project 01 - Level 5
+# Purpose: Demonstrate how salts affect password hashing.
+
+import hashlib
+import secrets
+
+password = "cyber123"
+
+salt = secrets.token_hex(16)
+
+salted_password = password + salt
+
+hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()
+
+print(f"Password: {password}")
+print(f"Salt: {salt}")
+print(f"Hash: {hashed_password}")
